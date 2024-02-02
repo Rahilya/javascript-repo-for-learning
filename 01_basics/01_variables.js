6fd6795aa11af9b3deb176629cd6466552b9b15f
Rahilya <87383514+Rahilya@users.noreply.github.com>
2024-02-02 11:58:01 +0000

    variable declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const employeeId = 166559;
+let employeeEmail = "rahil@lategeek.com";
+var employeePassword = "abcd";
+name ="lategeek";
+/*
+not to use var because of block scope
+*/
+console.log(name);
+console.table([employeeId, employeeEmail, employeePassword]);
